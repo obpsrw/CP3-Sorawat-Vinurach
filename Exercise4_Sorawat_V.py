a3ecac96177666ed3dee6f4a13ecde301409d5bf
obpsrw <Sorawat.vinu@gmail.com>
2022-03-07 12:36:29 +0700

    Rev.0

---
+++ Exercise4_Sorawat_V.py
@@ -0,0 +1,13 @@
+name = input("Your Name : ")
+print("Hello "+ name)
+Foundation_English = 81
+General_Business = 67
+Introduction_to_Computer_Systems = 78
+Computer_Programming = 86
+print("-"*20)
+print("     Your Score")
+print("-"*20)
+print("Foundation English = ",Foundation_English,"point")
+print("General Business = ",General_Business,"point")
+print("Introduction to Computer Systems = ",Introduction_to_Computer_Systems,"point")
+print("Computer Programming = ",Computer_Programming,"point")
